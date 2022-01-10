f3417532f534582951b002366ccb60e3eef803c7
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-01-09 19:27:18 -0500

    commit tree 689edb6462de221c37e52f2d0d912338b199b4c8

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+
+import React, { useState, useEffect } from "react";
+
+function App(){
+    const [image, setImage] = useState(null)
+
+    useEffect( () =>
+        {fetch('https://dog.ceo/api/breeds/image/random')
+        .then(r => r.json())
+        .then(data => setImage(data.message))},
+        []
+    )
+
+    console.log(image)
+
+    if (!image){
+        return <h2>Loading</h2>
+    }
+
+    return <img src={image} alt="A Random Dog"/>
+    
+}
+
+export default App
